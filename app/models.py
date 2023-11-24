ac3a5bf838e323a467a5d5728aeecdbb2bd8e86c
Romeo <romeoestoy0101@gmail.com>
2023-11-23 16:55:17 -0800

    Modify models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,6 @@ class Admin(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db .String(255), nullable=False)
-    profile_pic = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -18,10 +17,16 @@ class Data(db.Model):
     backup_container_status = db.Column(db.Integer, nullable=False)
     datetime = db.Column(db.DateTime, nullable=False)
 
+    def __repr__(self):
+        return '<Id %r>' % self.id
+
 class Log(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, unique=False, nullable=False)
     user_logged_in = db.Column(db.DateTime, nullable=False)
     user_logout = db.Column(db.DateTime, nullable=False)
     duration = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return '<Id %r>' % self.id
     
